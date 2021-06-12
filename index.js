b15651f67e2510636d2d9182062213314f969c56
Ashutosh Gowardhan <ashutosh.reso@gmail.com>
2021-06-12 22:25:25 +0530

    get and post request, uuid

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import usersRoutes from './routes/users.js'
+import usersRoutes from './routes/users.js';
 const app = express();
 const PORT = 5000;
 
-app.use(bodyParser.json());
-
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}));
 app.use('/users', usersRoutes)
 
 app.get('/', (req, res) => {
--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,31 @@
 import express from 'express';
+import { v4 as uuidv4 } from 'uuid';
+
 
 const router = express.Router();
 
+const users = [
+    {
+        firstName: "John",
+        lastName: "Doe",
+        age: 25
+    }
+]
 // all routes in here strt from 
-router.get('/', (res, req) => {
-    res.setEncoding('Hello');
+router.get('/', (req, res) => {
+    console.log(users)
+    res.send(users);
 });
+
+router.post('/', (req, res) => {
+    console.log("post route reached")
+    const user = req.body;
+    const userId = uuidv4();
+    const userWithId = { ...user, id: userId }
+    res.send("Post route reached")
+    console.log(user)
+    users.push(userWithId)
+    console.log(users)
+})
 
 export default router;
